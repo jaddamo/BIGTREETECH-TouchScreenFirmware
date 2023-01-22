837a632e18286b0a9dd042ec3fc5ded01461c3e3
Sourcery AI <>
2023-01-22 21:15:41 +0000

    'Refactored by Sourcery'

--- buildroot/scripts/auto_copy_firmware.py
+++ buildroot/scripts/auto_copy_firmware.py
@@ -6,10 +6,13 @@ def copy_firmware(source, target, env):
     firmware_name = "/" + env['PROGNAME'] + ".bin"
     firmware_path = env.subst('$BUILD_DIR') + firmware_name
     build_flags = env.ParseFlags(env['BUILD_FLAGS'])
-    flags = {k: v for (k, v) in build_flags.get("CPPDEFINES")}
+    flags = dict(build_flags.get("CPPDEFINES"))
     target_directory = flags.get("BINARY_DIRECTORY")
-    if target_directory == None:
-        shutil.copyfile(firmware_path, "Copy to SD Card root directory to update/" + firmware_name)
+    if target_directory is None:
+        shutil.copyfile(
+            firmware_path,
+            f"Copy to SD Card root directory to update/{firmware_name}",
+        )
     else:
         shutil.copyfile(firmware_path, target_directory + firmware_name)
     print("Done.")
--- buildroot/scripts/short_out_filename.py
+++ buildroot/scripts/short_out_filename.py
@@ -5,12 +5,14 @@ def make_short_file(source, target, env):
     print("Generating firmware with short file name...")
     buildDir = env.subst('$BUILD_DIR')
     build_flags = env.ParseFlags(env['BUILD_FLAGS'])
-    flags = {k: v for (k, v) in build_flags.get("CPPDEFINES")}
+    flags = dict(build_flags.get("CPPDEFINES"))
     filename = flags.get("BINARY_FILENAME")
-    if filename == None:
+    if filename is None:
         filename = flags.get("HARDWARE") + "." + flags.get("SOFTWARE_VERSION")
     filename_short = flags.get("HARDWARE_SHORT") + flags.get("SOFTWARE_VERSION_SHORT")
-    shutil.copyfile(buildDir + "/" + filename + ".bin", buildDir + "/" + filename_short + ".new")
+    shutil.copyfile(
+        f"{buildDir}/{filename}.bin", f"{buildDir}/{filename_short}.new"
+    )
     print("Done.")
 
 env.AddPostAction("buildprog", make_short_file)
